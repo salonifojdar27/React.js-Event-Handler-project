bff5f5e7d2d6995a60a9069f68e2def90ea582ab
salonifojdar27 <salonifojdar27@gmail.com>
2025-07-10 13:00:06 +0530

    complete

--- Fetch data/src/App.jsx
+++ Fetch data/src/App.jsx
@@ -8,11 +8,12 @@ function App() {
   const [posts, setposts] = useState([])
   const [loading, setloading] = useState(false)
   const [error, seterror] = useState("")
+  const [page, setpage] = useState(1)
 
   const getDatafromserver = async () => {
     setloading(true)
     try {
-      const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`)
       setloading(false)
       setposts(res.data)
 
@@ -25,18 +26,23 @@ function App() {
   }
   useEffect(() => {
     getDatafromserver()
-  }, [])
+  }, [page])
   return loading ? "Loading" : error ? "Network Error" : (
     <div className='container'>
-      <h1>posts are here...</h1>
+      <h1>Posts are here...</h1>
       <hr />
       {posts.map((el) => (
         <div className='details'>
-          <p>{el.id}</p>
-          <p>{el.title}</p>
+          <h4>{el.id}</h4>
+          <h3>{el.title}</h3>
           <p>{el.body}</p>
         </div>
       ))}
+      <div className='buttons'>
+        <button disabled={page == 1} onClick={() => setpage(page - 1)}>Preview</button>
+        <h2>{page}</h2>
+        <button onClick={() => setpage(page + 1)} disabled={page == posts.length}>Next</button>
+      </div>
     </div>
   )
 }
